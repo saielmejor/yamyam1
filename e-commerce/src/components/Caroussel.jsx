820f064d81538cc7c68d42264f93f755ad349196
Saiken Ho <saikenhh@gmail.com>
2023-11-22 21:25:55 -0500

    added img fluid

--- e-commerce/src/components/Caroussel.jsx
+++ e-commerce/src/components/Caroussel.jsx
@@ -6,21 +6,21 @@ export default function Caroussel() {
     <div className='my_carousel_main'>
     <Carousel>
     <Carousel.Item>
-      <img className="photo" src="/images/ricebowl.png" alt="rice" />
+      <img class="img-fluid" src="/images/ricebowl.png" alt="rice" />
       <Carousel.Caption>
         <h3> Your bowl of choice</h3>
         <p>Hand-crafted and Delicious</p>
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
-    <img className="photo" src="/images/arepa.jpg" alt="arepas" />
+    <img class="img-fluid" src="/images/arepa.jpg" alt="arepas" />
       <Carousel.Caption>
         <h3> Choose your Filling</h3>
         <p>Bite into a Delicious Experience</p>
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
-    <img className='photo' src="/images/rice.jpg" alt="empanadas" />
+    <img class="img-fluid" src="/images/rice.jpg" alt="empanadas" />
       <Carousel.Caption>
         <h3>Let's Have Empanadas</h3>
         <p>
--- e-commerce/src/pages/Advertisement.jsx
+++ e-commerce/src/pages/Advertisement.jsx
@@ -2,27 +2,40 @@ import React from "react";
 
 import data from "../data";
 export default function Advertisement() {
-const {products}=data
+  const { products } = data;
   return (
-    <div id="advertisement"> 
-    <div class="container d-flex justify-content-center ">
-      
+    <div id="advertisement">
+      <div class="container d-flex justify-content-center ">
         <div class="row m-5">
           <div class="col-sm mx-5 ">
-          <div class="container border border-primary "><img className="popular-image" src="/images/cater.png" alt="pabellon" />
-          <div className="popular-food-dish"> <h4>Fan Favorite Pabellon</h4></div> </div>
-          
+            <div class="container border border-dark ">
+              <img
+                className="popular-image"
+                src="/images/cater.png"
+                alt="pabellon"
+              />
+              <div className="popular-food-dish">
+                {" "}
+                <h4>Fan Favorite Pabellon</h4>
+              </div>{" "}
+            </div>
           </div>
           <div class="col-sm mx-5 ">
-          <img className="popular-image" src="/images/cachapa-fresh.png" alt="arepa" />
-          <div className="popular-food-dish"> <h4>Fan Favorite Arepas</h4></div>
+            <div class="container border border-dark ">
+              {" "}
+              <img
+                className="popular-image"
+                src="/images/cachapa-fresh.png"
+                alt="arepa"
+              />
+              <div className="popular-food-dish">
+                {" "}
+                <h4>Fan Favorite Arepas</h4>
+              </div>
+            </div>
           </div>
-        
         </div>
       </div>
-      
-    
-
     </div>
   );
 }
